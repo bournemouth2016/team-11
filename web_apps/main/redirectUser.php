366e8d3ce0b95e32ff090fd90a97b9cde5e49e0c
Medardas <zmedardas@gmail.com>
2016-11-19 06:45:32 +0000

    Redirection for different users

--- web_apps/main/redirectUser.php
+++ web_apps/main/redirectUser.php
@@ -3,4 +3,10 @@ session_start();
 if($_SESSION['type'] == "normal"){
 	header('Location:panel.php');
 }
+else if($_SESSION['type'] == "admin"){
+	header('Location:../rnli/index.php');
+}
+else if($_SESSION['type'] == "rnli_staff"){
+	header('Location:../rnli/index.php');
+}
 ?>
