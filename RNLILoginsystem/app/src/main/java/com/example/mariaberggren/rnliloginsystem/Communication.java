0ba63c779bf3b112c9637f57b39187031d4e51f6
mshahi <MariaBerggren@Marias-MacBook-Pro.local>
2016-11-19 05:19:44 +0000

    Login System

---
+++ RNLILoginsystem/app/src/main/java/com/example/mariaberggren/rnliloginsystem/Communication.java
@@ -0,0 +1,62 @@
+package com.example.mariaberggren.rnliloginsystem;
+
+/**
+ * Created by MariaBerggren on 19/11/2016.
+ */
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Communication {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+
+    // HTTP POST request
+    public String sendPost(String address, String urlParameters) throws Exception {
+
+        String url = "http://budde.ws/web_apps/main/"+address;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        //String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+        return(response.toString());
+
+    }
+
+}
