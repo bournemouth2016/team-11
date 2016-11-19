30bec17718ecbffe5ad44eab9b9156a0cab5ae02
Eavan Pattie <eavan@lyse.net>
2016-11-19 05:44:47 +0000

    added blue header bar

--- RNLI/app/src/main/java/com/example/mariaberggren/rnli/MainActivity.java
+++ RNLI/app/src/main/java/com/example/mariaberggren/rnli/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.buttons_main);
+        setContentView(R.layout.activity_main);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
